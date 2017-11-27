3826ee8928593471bda714b32a7f676047304709
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-11-27 10:25:14 +0100

    Oops update code.

--- contracts/FundRaise.sol
+++ contracts/FundRaise.sol
@@ -1,5 +1,38 @@
-pragma solidity ^0.4.13;
+pragma solidity ^0.4.17;
 
 contract FundRaise {
+    address public owner;
+    bool paused;
 
+    // modifiers
+    modifier onlyOwner() {
+        assert(owner == msg.sender);
+        _;
+    }
+
+    modifier whenNotPaused(){
+        require(!paused);
+        _;
+    }
+
+    // @dev constructor function. Sets contract owner
+    function FundRaise() {
+        owner = msg.sender;
+    }
+
+    // fallback function to accept ETH into contract.
+    function () whenNotPaused payable {
+    }
+
+    function pause() public onlyOwner {
+        paused = true;
+    }
+
+    function unpause() public onlyOwner {
+        paused = false;
+    }
+
+    function removeFunds() public {
+        owner.transfer(this.balance);
+    }
 }
--- test/fundRaise.js
+++ test/fundRaise.js
@@ -1,6 +1,6 @@
 const FundRaise = artifacts.require('./FundRaise.sol')
 
-contract('FundRaise', function ([owner]) {
+contract('FundRaise', function ([owner, donor]) {
     let fundRaise
 
     beforeEach('setup contract for each test', async function () {
@@ -9,5 +9,42 @@ contract('FundRaise', function ([owner]) {
 
     it('has an owner', async function () {
         assert.equal(await fundRaise.owner(), owner)
+    })
+
+    it('accepts funds', async function () {
+        await fundRaise.sendTransaction({ value: 1e+18, from: donor })
+
+        const fundRaiseAddress = await fundRaise.address
+        assert.equal(web3.eth.getBalance(fundRaiseAddress).toNumber(), 1e+18)
+    })
+
+    it('is able to pause and unpause fund activity', async function () {
+        await fundRaise.pause()
+
+        try {
+            await fundRaise.sendTransaction({ value: 1e+18, from: donor })
+            assert.fail()
+        } catch (error) {
+            assert(error.toString().includes('invalid opcode'), error.toString())
+        }
+        const fundRaiseAddress = await fundRaise.address
+        assert.equal(web3.eth.getBalance(fundRaiseAddress).toNumber(), 0)
+
+        await fundRaise.unpause()
+        await fundRaise.sendTransaction({ value: 1e+18, from: donor })
+        assert.equal(web3.eth.getBalance(fundRaiseAddress).toNumber(), 1e+18)
+    })
+
+    it('permits owner to receive funds', async function () {
+        await fundRaise.sendTransaction({ value: 1e+18, from: donor })
+        const ownerBalanceBeforeRemovingFunds = web3.eth.getBalance(owner).toNumber()
+
+        const fundRaiseAddress = await fundRaise.address
+        assert.equal(web3.eth.getBalance(fundRaiseAddress).toNumber(), 1e+18)
+
+        await fundRaise.removeFunds()
+
+        assert.equal(web3.eth.getBalance(fundRaiseAddress).toNumber(), 0)
+        assert.isAbove(web3.eth.getBalance(owner).toNumber(), ownerBalanceBeforeRemovingFunds)
     })
 })
